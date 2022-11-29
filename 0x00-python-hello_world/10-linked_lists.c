516730d71448aa11a90c62ddcabb447284b6f67e
babajallow91 <momodousalieu.jallow@gmail.com>
2022-11-29 02:56:13 -0800

    gotask

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -10,10 +10,12 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
-	unsigned int n; /* number of nodes */
+	unsigned int n;
+/* number of nodes */
 
 	current = h;
 	n = 0;
+
 	while (current != NULL)
 	{
 		printf("%i\n", current->n);
@@ -28,20 +30,20 @@ size_t print_listint(const listint_t *h)
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to a pointer of the start of the list
  * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
+ * * Return: address of the new element or NULL if it fails
  */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
 	return (new);
 }
 
@@ -53,7 +55,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 void free_listint(listint_t *head)
 {
 	listint_t *current;
-
 	while (head != NULL)
 	{
 		current = head;
