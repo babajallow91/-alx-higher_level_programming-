a8671bb819dbac160f1f06bdca79269ae8c3f28c
babajallow91 <momodousalieu.jallow@gmail.com>
2023-03-24 09:50:47 -0700

    fix-14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Check that the script is being called correctly
+    if (len(sys.argv) != 4):
+        print("Usage: {} username password db_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Parse command line arguments
+    username, password, db_name = sys.argv[1:]
+
+    # Create a connection to the MySQL server running on localhost at port 3306
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+           sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+       )
+
+    # Create the table(s) associated with the defined classes
     Base.metadata.create_all(engine)
+
+    # Create a session factory bound to the engine
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
+
+    # Query all State objects from the database and sort them by id
+    results = session.query(State, City).join(City).order_by(City.id)
+
+    # Display the results
+    for state, city in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    # Close the session
+    session.close()
